bc6b6154f19a5d130ef1779a401c9c58baa569a5
Walter Berger <W.berger89@gmx.at>
2023-08-06 12:25:57 +0200

    renaming

--- src/Microservices/AccountService/Features/DeleteUser/DeleteUserCmdHandler.cs
+++ src/Microservices/AccountService/Features/DeleteUser/DeleteUserCmdHandler.cs
@@ -1,12 +1,16 @@
-﻿namespace AccountService.Features.DeleteUser;
+﻿using Contracts.Events;
+
+namespace AccountService.Features.DeleteUser;
 
 public class DeleteUserCmdHandler : IRequestHandler<DeleteUserCmd, Unit>
 {
     private readonly DatabaseContext _databaseContext;
+    private readonly IPublishEndpoint _publisher;
 
-    public DeleteUserCmdHandler(DatabaseContext databaseContext)
+    public DeleteUserCmdHandler(DatabaseContext databaseContext, IPublishEndpoint publisher)
     {
         _databaseContext = databaseContext;
+        _publisher = publisher;
     }
 
     public async Task<Unit> Handle(DeleteUserCmd request, CancellationToken cancellationToken)
@@ -14,6 +18,11 @@ public class DeleteUserCmdHandler : IRequestHandler<DeleteUserCmd, Unit>
         //check if user exists
         var user = await _databaseContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken)
             ?? throw new NotFoundException(ErrorDetails.UserNotFound);
+
+        // create an event which will be publisherd to rabbitmq broker
+        var userDeletedEvent = new UserDeletedEvent(Email: user.Email);
+
+        await _publisher.Publish(userDeletedEvent, cancellationToken);
 
         // remove user from database
         _databaseContext.Users.Remove(user);
---
+++ src/Microservices/IdentityService/Consumers/UserDeletedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Contracts.Events;
+
+namespace IdentityService.Consumers;
+
+public class UserDeletedConsumer : IConsumer<UserDeletedEvent>
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public UserDeletedConsumer(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task Consume(ConsumeContext<UserDeletedEvent> context)
+    {
+        var message = context.Message;
+
+        var user = await _userManager.FindByEmailAsync(message.Email);
+
+        if (user == null)
+        {
+            return;
+        }
+
+        await _userManager.DeleteAsync(user);
+    }
+}
