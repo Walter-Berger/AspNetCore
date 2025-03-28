76aa5114177ea6d3566dc97ba21a653ad0a91ed8
Walter <w.berger89@gmx.at>
2025-03-28 15:31:25 +0100

    added more tests

--- tests/AccountService.Tests/Integration/CreateUserTest.cs
+++ tests/AccountService.Tests/Integration/CreateUserTest.cs
@@ -1,4 +1,5 @@
 using AccountService.Features.CreateUser;
+using FluentValidation;
 
 namespace AccountService.Tests.Integration;
 
@@ -9,7 +10,7 @@ public class CreateUserTest : BaseIntegrationTest
     }
 
     [Fact]
-    public async Task Create_ShouldReturnOk_WhenUserIsValid()
+    public async Task Create_ShouldAddUser_WhenDataIsValid()
     {
         // Arrange
         var command = new CreateUserCmd("john.xina@gmx.at", "John", "Xina");
@@ -20,5 +21,44 @@ public class CreateUserTest : BaseIntegrationTest
         // Assert
         var user = DbContext.Users.FirstOrDefault(p => p.Email == command.Email);
         Assert.NotNull(user);
+    }
+
+    [Fact]
+    public async Task Create_ShouldThrowValidationError_WhenEmailIsInvalid()
+    {
+        // Arrange
+        var command = new CreateUserCmd("john.xina", "John", "Xina");
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<ValidationException>(Action);
+    }
+
+    [Fact]
+    public async Task Create_ShouldThrowValidationError_WhenFirstNameIsEmpty()
+    {
+        // Arrange
+        var command = new CreateUserCmd("john.xina@gmx.at", "", "Xina");
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<ValidationException>(Action);
+    }
+
+    [Fact]
+    public async Task Create_ShouldThrowValidationError_WhenLastNameIsEmpty()
+    {
+        // Arrange
+        var command = new CreateUserCmd("john.xina@gmx.at", "John", "");
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<ValidationException>(Action);
     }
 }
